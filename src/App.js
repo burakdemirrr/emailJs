48edbb1da40880be775a4fec5580412419c19544
Burak <mburak.demir.059@gmail.com>
2022-08-28 18:26:48 +0300

    Email Component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import  {ContactUs} from "./Email"
+
+function App() {
+  return (
+    <div className="App">
+        <ContactUs/>
+    </div>
+  );
+}
+
+export default App;
